aa0e8fd15e2c40b02af5d3567efafd2e9cba5958
mnshoemake <themikeshoemake@gmail.com>
2020-12-05 14:24:33 -0800

    Add project files.

---
+++ Hephaestus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hephaestus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
